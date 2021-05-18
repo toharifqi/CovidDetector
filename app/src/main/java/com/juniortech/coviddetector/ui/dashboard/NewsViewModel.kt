8f39c08d17e036e94aadc27a014bb5f079a35330
toharifqi <rifqinaufaltohari@gmail.com>
2021-05-18 08:54:36 +0700

    Initial commit from rifqi

---
+++ app/src/main/java/com/juniortech/coviddetector/ui/dashboard/NewsViewModel.kt
@@ -0,0 +1,13 @@
+package com.juniortech.coviddetector.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NewsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
